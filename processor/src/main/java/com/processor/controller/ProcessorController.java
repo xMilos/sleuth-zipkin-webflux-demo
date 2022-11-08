0d6b9fcffee63e85286b81f4786d588dc60791b3
Milosh Nikolikj <m-i-l-o-s@hotmail.com>
2022-11-08 21:07:06 +0100

    make sleuth work

--- processor/src/main/java/com/processor/controller/ProcessorController.java
+++ processor/src/main/java/com/processor/controller/ProcessorController.java
@@ -1,5 +1,6 @@
 package com.processor.controller;
 
+import java.time.Duration;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,30 +8,32 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
 import reactor.core.publisher.Flux;
-
-import java.time.Duration;
 
 @Slf4j
 @RestController
 public class ProcessorController {
 
-    public static final String EMITTER_ENDPOINT = "localhost:8083/emitter";
+    public static final String EMITTER_ENDPOINT = "http://localhost:8083/emitter";
+    private final WebClient.Builder webClientBuilder;
+
+    public ProcessorController(Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
 
     @GetMapping("processor")
     public Flux<Event> processEvents() {
         log.info("processing some strings");
-        return WebClient.create().get()
-                .uri(uriBuilder -> uriBuilder.path(EMITTER_ENDPOINT)
-                        .build())
+        return webClientBuilder.baseUrl(EMITTER_ENDPOINT).build().get()
                 .retrieve()
                 .bodyToFlux(Event.class)
                 .map(v -> {
-                    log.info("In flatMap operator, processing {} ", v);
+                    log.info("In map operator, processing {} ", v);
                     if(v.getNumber() % 2 == 0){
-                        v.setInfo("Even: " + v);
+                        v.setInfo("Even: " + v.getInfo());
                     } else {
-                        v.setInfo("Odd: " + v);
+                        v.setInfo("Odd: " + v.getInfo());
                     }
                     return v;
                 }).delayElements(Duration.ofMillis(100));
--- receiver/src/main/java/com/receiver/controller/ReceiverController.java
+++ receiver/src/main/java/com/receiver/controller/ReceiverController.java
@@ -1,29 +1,33 @@
 package com.receiver.controller;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
 import reactor.core.publisher.Mono;
-
-import java.time.Duration;
-import java.util.List;
 
 @Slf4j
 @RestController
 public class ReceiverController {
 
-    public static final String PROCESSOR_ENDPOINT = "localhost:8084/processor";
+    private final WebClient.Builder webClientBuilder;
+    public static final String PROCESSOR_ENDPOINT = "http://localhost:8084/processor";
+
+    @Autowired
+    public ReceiverController(Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
 
     @GetMapping("receiver")
     public Mono<List<Event>> receiveSomeString() {
         log.info("received some strings");
-        return WebClient.create().get()
-                .uri(uriBuilder -> uriBuilder.path(PROCESSOR_ENDPOINT)
-                        .build())
+        return webClientBuilder.baseUrl(PROCESSOR_ENDPOINT).build().get()
                 .retrieve()
                 .bodyToFlux(Event.class)
                 .collectList();
